59615aa64770cd2f1be19ad6b6cef3b648a06b3e
el3raky <kameladel339@gmail.com>
2024-12-29 11:01:48 +0200

    update get all products & user by email

--- routes/user-route.js
+++ routes/user-route.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 // Routes
 router.get("/users", getAllUsers);
-router.get("/userByEmail" , getUserByEmail)
+router.post("/userByEmail" , getUserByEmail)
 router.get("/users/:id", getUserById);
 router.post("/users", createUser);
 router.put("/users/:id", updateUser);
