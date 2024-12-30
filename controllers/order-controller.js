9066b5913affb59cc36a314fbaecab8e850d440c
hazemr2121 <ha.ahmed8996@gmail.com>
2024-12-30 20:37:46 +0200

    updated order

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -38,15 +38,13 @@ exports.getOrderById = async (req, res) => {
 // Update order by ID
 exports.updateOrderById = async (req, res) => {
   try {
-    const updatedOrder = await Order.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
+    const updatedOrder = await Order.findById(req.params.orderId);
     if (!updatedOrder) {
       return res.status(404).json({ message: "Order not found" });
     }
-    res.status(200).json(updatedOrder);
+    updatedOrder.status = req.body.status;
+    await updatedOrder.save();
+    return res.status(200).json(updatedOrder);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
